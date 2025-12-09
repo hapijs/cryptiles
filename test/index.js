e6ac5ca59f50f273d9c8f4a180b17ebf8c50b46f
Danilo Alonso <damusix@gmail.com>
2025-12-09 11:48:11 -0500

    fix: 🐛 Add timing-safe comparison tests and update API docs

--- test/index.js
+++ test/index.js
@@ -90,4 +90,52 @@ describe('fixedTimeComparison()', () => {
         expect(Cryptiles.fixedTimeComparison('', '')).to.be.true();
         expect(Cryptiles.fixedTimeComparison('asdas', 'asdasd')).to.be.false();
     });
+
+    it('should not throw if buffer size differs', () => {
+
+        expect(() => Cryptiles.fixedTimeComparison('a', 'ab')).to.not.throw();
+        expect(() => Cryptiles.fixedTimeComparison('abc', 'a')).to.not.throw();
+        expect(() => Cryptiles.fixedTimeComparison('', 'a')).to.not.throw();
+        expect(() => Cryptiles.fixedTimeComparison('a', '')).to.not.throw();
+    });
+
+    it('should provide constant time regardless of the size of the right-most argument', { timeout: 10000 },  () => {
+
+        // Test that comparison time is based on left argument, not right
+        // When lengths differ, we compare left to itself (constant time based on left)
+        const largeLeft = 'a'.repeat(100000);
+        const smallLeft = 'b'.repeat(10);
+        const smallRight = 'x'.repeat(10);
+        const largeRight = 'y'.repeat(100000);
+
+        const iterations = 10000;
+
+        // Warm up
+        for (let i = 0; i < 1000; ++i) {
+            Cryptiles.fixedTimeComparison(largeLeft, smallRight);
+            Cryptiles.fixedTimeComparison(smallLeft, largeRight);
+        }
+
+        // Measure large left + small right (timing should be based on large left)
+        const startLargeLeft = process.hrtime.bigint();
+        for (let i = 0; i < iterations; ++i) {
+            Cryptiles.fixedTimeComparison(largeLeft, smallRight);
+        }
+
+        const endLargeLeft = process.hrtime.bigint();
+        const largeLeftTime = Number(endLargeLeft - startLargeLeft);
+
+        // Measure small left + large right (timing should be based on small left)
+        const startSmallLeft = process.hrtime.bigint();
+        for (let i = 0; i < iterations; ++i) {
+            Cryptiles.fixedTimeComparison(smallLeft, largeRight);
+        }
+
+        const endSmallLeft = process.hrtime.bigint();
+        const smallLeftTime = Number(endSmallLeft - startSmallLeft);
+
+        // Large left should take longer than small left, proving timing is based on left
+        // Even though small left has a much larger right argument
+        expect(largeLeftTime).to.be.above(smallLeftTime);
+    });
 });
